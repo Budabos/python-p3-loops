0ce1e625dad7a492eaf6ad71eed7d75bd9da6b70
caroline opiyo <opiyocaroline20@gmail.com>
2023-11-21 23:53:15 +0300

    python-p3-loops

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,32 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+    i = 10
+    
+    while i > 0:
+        print(i)
+        i -= 1
+        print("Happy New Year!")
+        
+
 
 def square_integers(int_list):
     # code goes here!
-    pass
+
+    square_integers = [num ** 2 for num in int_list]
+    return square_integers
 
 def fizzbuzz():
     # code goes here!
-    pass
+    
+    for num in range(1, 101):
+        output = ""
+        if num % 3 == 0:
+            output += "Fizz"
+        if num % 5 == 0:
+            output += "Buzz"
+        print(output or num)
+
+# Call the function to see the output
+fizzbuzz()
+
